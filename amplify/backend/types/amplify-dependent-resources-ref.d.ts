fc162ca097f66cad101d39c332102de18cafc6c6
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2023-11-30 21:35:39 -0600

    Added the Amplify folder

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "scrumlordstailwind": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
